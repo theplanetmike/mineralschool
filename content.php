09d6b57547f25cdb9ee71b3d4f25cb96ee9c92e0
d <ddoner01@seattlecentral.edu>
2014-10-21 19:44:11 -0400

    Added Categories to content

--- content.php
+++ content.php
@@ -5,5 +5,6 @@
   the_title('<h2><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
   endif; ?>
   <p>Author: <?php the_author(); ?> Date: <?php the_date(); ?></p>
+  <p>Categories: <?php the_category(', '); ?></p>
   <?php the_content(); ?>
 </div>
