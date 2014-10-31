c1bee96eaea5cfbf5981acf42a3d5795f1e78fc9
d <ddoner01@seattlecentral.edu>
2014-10-31 14:41:56 -0400

    updated content-page and content templates to fix merge errors

--- content-page.php
+++ content-page.php
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-<div class="page-container">
 <?php the_title('<h2>', '</h2>'); ?>
-=======
-<div class="post-container">
   	<!-- content-page.php template -->
 	<?php the_title('<h2>', '</h2>'); ?>
->>>>>>> 5a68f02f7174ae0c7d121a0abf0961537698830c
 
 	<?php the_content(); ?>
 </div>
--- content.php
+++ content.php
@@ -17,24 +17,3 @@ the_excerpt();
 <?php
 endif;  
 ?>
-</div><!-- end .post-container -->
-=======
-  	<!-- content.php template -->
-	<?php if (is_single() :
-	  the_title('<h2>', '</h2>' );
-	else:
-	  the_title('<h2><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
-	endif; ?>
-	<div class="post-meta">
-		<p>Author: <?php the_author(); ?> Date: <?php the_date(); ?></p>
-	    <p>Categories: <?php the_category(', '); ?></p>
-	</div>
-	<?php if (is_single() ) :
-		the_content();
-	else:
-		the_excerpt();
-	endif;  
-	?>
-</div>
-<!-- end .post-container -->
->>>>>>> 5a68f02f7174ae0c7d121a0abf0961537698830c
