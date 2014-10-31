9cb152b51b7b59dcc6fd2414dc161f5a91e9b66c
d <ddoner01@seattlecentral.edu>
2014-10-31 17:16:47 -0400

    Added styling for pages,

--- functions.php
+++ functions.php
@@ -56,3 +56,10 @@ function changed_excerpt_more( $more ) {
 	return '...';
 }
 add_filter('excerpt_more', 'changed_excerpt_more');
+
+// function for removing paragraph tags from wrapping around inserted images in posts. snippet credit http://css-tricks.com/snippets/wordpress/remove-paragraph-tags-from-around-images/
+function filter_ptags_on_images($content){
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+
+add_filter('the_content', 'filter_ptags_on_images');
