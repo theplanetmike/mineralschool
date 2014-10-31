53d1b9277662bcdf4314cabe1783340ab26570c0
theplanetmike <michaelbowers@Mikebook-Air.local>
2014-10-31 12:42:03 -0700

    adds page-home.php

---
+++ page-home.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<!-- page-home.php template -->
+
+<div class="main home">
+  <p>testerino</p>
+  <section class="main-content">
+    <?php 
+      if ( have_posts() ) :
+      	while ( have_posts() ) :
+            the_post();
+            get_template_part('content', 'page');
+
+            if (comments_open() ) {
+              comments_template();
+            }
+      	endwhile;
+      endif;
+    ?>
+  </section>
+  <?php get_sidebar( 'primary' ); ?>
+</div>
+
+<?php get_footer(); ?>
