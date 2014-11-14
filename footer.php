579d7037dc524ebe5059a1fd95119fbb5dcbd136
d <ddoner01@seattlecentral.edu>
2014-11-13 23:09:01 -0500

    Fixes issues with registering menus in header and footer template

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <!-- BEGIN footer.php template -->
     <footer>
       <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu' => 'Main Footer Menu')); ?> 
-      <p>Copyright &#169; 2014 Mineral School</p>
+      <p>Copyright &#169; <?php the_date(' Y '); ?> <?php bloginfo('name'); ?></p>
       <?php get_sidebar( 'footer-widgets' ); ?>
     </footer>
 	<?php wp_footer(); ?>
