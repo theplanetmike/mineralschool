69dacab1af615cfea1c01932aa19155be9814605
d <ddoner01@seattlecentral.edu>
2014-12-02 23:03:46 -0500

    Adds background-image for widget- headers

--- searchform.php
+++ searchform.php
@@ -3,5 +3,5 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
 		<input type="search" class="search-field" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 	</label>
-	<img class="search-icon" src="<?php bloginfo('template_url'); ?>/images/search-icon.png" alt="Search icon" >
+	<img class="search-icon" src="<?php bloginfo('template_url'); ?>/images/white-search-icon.png" alt="Search icon" >
 </form>
