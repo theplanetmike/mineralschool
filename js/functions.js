74e780b9afa6c558b96dd72eca106925f3c658b2
d <ddoner01@seattlecentral.edu>
2014-12-05 21:59:20 -0500

    Adds functionality for showing and hiding mobile menu. Also adds padding to header donate button

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,23 @@
 var searchAnim = function() {
     $('input.search-field').animate( {width: 'toggle'} );
-  console.log('hi');
+}
+
+var mobileClick = function(e) {
+  navLink = $(this).attr("href");
+  if ($(navLink).is(':hidden')) {
+    $(navLink).css('display', 'block');
+    $(navLink).css('opacity', '1');
+    $(navLink).css('visibility', 'visible');
+  } else {
+    $(navLink).css('display', 'none');
+    $(navLink).css('opacity', '0');
+    $(navLink).css('visibility', 'hidden');
+  }
+  e.preventDefault();
 }
 
 $(document).ready( function() {
 $('input.search-field').hide();
 $('img.search-icon').click( searchAnim );
+$('#three-line-menu').click( mobileClick );
 });
